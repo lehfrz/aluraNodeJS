929e72b1dc749b6b9119bc8fc69220297097a8e8
Alexandre França <leh.fr@hotmail.com>
2022-06-07 22:54:09 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,31 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuante = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+//NAN -> not a number
+
+const alura = 'Alura';
+
+console.log(alura * primeiroNumero);
+
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
+
+var a = 0
+var b = 0
+console.log(a/b) // NaN
---
+++ type-string.js
@@ -0,0 +1,22 @@
+const texto1 = "Olá, mundo!";
+const text2 = 'Olá, mundo!';
+const stringDeNumero = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+//concatenacao (+)
+
+console.log(citacao+meuNome);
+
+//template string ou template literal
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
